b967c6a2c49ee83fe7709728d02e39a60698b366
lhl3176577 <443065922@qq.com>
2015-10-08 14:10:40 -0400

    first-publish

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET ABOUT page */
+router.get('/about', function(req, res, next) {
+   res.render('about'); 
+});
+
+router.get('/contact', function(req, res, next) {
+   res.render('contact'); 
+});
+
+router.get('/project', function(req, res, next) {
+   res.render('project'); 
+});
+
+router.get('/server', function(req, res, next) {
+   res.render('server'); 
+});
+
+
+
+module.exports = router;
